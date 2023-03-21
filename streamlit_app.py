72efbbfaf54ece8eb821f381a9e9d1a9ab871f9d
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 12:37:26 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,37 @@ selected = option_menu(
 
 
 if selected == "Grafico":
+     from pymongo import MongoCliente
+     import pandas as pd
+     import plotly.graph_objects as go
+
+     client = MongoClient("mongodb+srv://casper:casperKey@clustercrypto.6ydpkxh.mongodb.net/?retryWrites=true&w=majority")
+     db = client["CryptoData"]
+     collection = db["BTC/BUSD_1m"]
+
+     data_activo = pd.DataFrame(list(collection.find().limit(100)))
      st.title(f"Selecionado {selected}")
+
+     fig = go.Figure()
+
+     fig.add_trace(go.Candlestick(x=data_activo["datetime"], open=data_activo["open"], high=data_activo["high"], low=data_activo["low"], close=data_activo["close"]))
+     #fig.add_trace(go.Histogram(x=data_activo["volume"]))
+     fig.update_layout(
+            #xaxis_title='Tiempo',
+            #yaxis_title='Precio',
+
+            height = 750,
+            margin=dict(l=0, r=0, t=0, b=0,pad=0),
+            xaxis_rangeslider_visible=False)
+     fig.update_yaxes(automargin='left+top+right',ticklabelposition="inside")
+     #fig.update_xaxes(automargin='left+right')
+     configs = dict({'modeBarButtonsToAdd':['drawline',
+                                    'drawopenpath',
+                                    'drawcircle',
+                                    'drawrect',
+                                    'eraseshape',
+                                ],'scrollZoom': True})
+     st.plotly_chart(fig,use_container_width=True,config=configs)
+
 if selected == "Obtener datos":
      st.title(f"Selecionado {selected}")
