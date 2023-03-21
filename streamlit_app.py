26c9add80014b6852c08ff20b0933ac65890c47a
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 12:10:29 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,6 +9,17 @@ selected = option_menu(
           menu_icon="list",
           default_index=0,
           orientation="horizontal",
+          styles={
+                "container": {"padding": "0!important", "background-color": "#fafafa"},
+                "icon": {"color": "orange", "font-size": "25px"},
+                "nav-link": {
+                    "font-size": "25px",
+                    "text-align": "left",
+                    "margin": "0px",
+                    "--hover-color": "#eee",
+                },
+                "nav-link-selected": {"background-color": "green"},
+            },
      )
 
 
