5bc2b8175a1668cf5409f81b10b75b126a56c19e
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 12:17:19 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from streamlit_option_menu import option_menu
 selected = option_menu(
           menu_title=None,
           options=["Grafico","Obtener datos"],
-          icons=["graph-up","database-add"],
+          icons=["graph-up","graph-up"],
           menu_icon="list",
           default_index=0,
           orientation="horizontal",
