ec001839f1eb87d1f7c54fe956c1bd90bff6cdbc
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 11:58:49 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,20 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
+
+# Sidebar menu
+
+with st.sidebar:
+     selected = option_menu(
+          menu_title="Menu Principal",
+          options=["Obtener datos", "Graficos"],
+     )
+
+if selected == "Obtener datos":
+     st.title(f"Selecionado {selected}")
+if selected == "Graficos":
+     st.title(f"Selecionado {selected}"
+
+
 st.title("Proyecto X")
 menu = st.selectbox("Menu Principal",("Obtener datos", "Graficos"))
 st.write("Has selecionado", menu)
