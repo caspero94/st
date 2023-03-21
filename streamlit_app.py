62cd91a8eadd1a8b956dccf58f77f67a36f43aec
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 11:52:43 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,4 @@
 import streamlit as st
-st.header('st.button')
-
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+st.title("Proyecto X")
+menu = st.selectbox("Menu Principal",("Obtener datos", "Graficos"))
+st.write("Has selecionado", menu)
