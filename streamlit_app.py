e796901a58a841969577d5708b622c30aec0d34f
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-21 12:06:49 -0500

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,25 @@ from streamlit_option_menu import option_menu
 with st.sidebar:
      selected = option_menu(
           menu_title="Menu Principal",
-          options=["Graficos","Obtener datos"],
+          options=["Grafico","Obtener datos"],
           icons=["graph-up","database",],
           menu_icon="list",
           default_index=0
      )
 
+# Horizontal menu
 
-if selected == "Graficos":
+selected = option_menu(
+          menu_title="Menu Principal",
+          options=["Grafico","Obtener datos"],
+          icons=["graph-up","database",],
+          menu_icon="list",
+          default_index=0,
+          orientation="horizontal",
+     )
+
+
+if selected == "Grafico":
      st.title(f"Selecionado {selected}")
 if selected == "Obtener datos":
      st.title(f"Selecionado {selected}")
