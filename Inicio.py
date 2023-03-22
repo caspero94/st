d88e7218df118bacfea174fa405173c647d4746e
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 10:11:45 -0500

    update

--- Inicio.py
+++ Inicio.py
@@ -1,7 +1,6 @@
 # Resources
 import streamlit as st
-from streamlit_option_menu import option_menu
-from streamlit_extras.switch_page_button import switch_page
+import menu
 #variables
 page_title = "Proyect X"
 page_icon = ":graph-up:"
@@ -26,35 +25,6 @@ st.markdown("""
     
 
 
-# Horizontal menu
-selected = option_menu(
-          menu_title=None,
-          options=["Inicio","Graficos","Obtener datos"],
-          icons=["house","graph-up","file-bar-graph-fill"],
-          menu_icon="list",
-          default_index=0,
-          orientation="horizontal",
-          styles={
-                "container": {"padding": "0!important", "background-color": "#363636"},
-                "icon": {"color": "white", "font-size": "25px"},
-                "nav-link": {
-                    "font-size": "25px",
-                    "text-align": "center",
-                    "margin": "0px",
-                    "--hover-color": "#8a0000",
-                },
-                "nav-link-selected": {"background-color": "#470000"},
-            },
-     )
-if selected == "Inicio":
-    
-    st.title(f"Selecionado {selected}")
-if selected == "Graficos":
-    switch_page("Graficos")
-    st.title(f"Selecionado {selected}")
 
-if selected == "Obtener datos":
-    switch_page("Graficos")
-    st.title(f"Selecionado {selected}")
 
 
