a950a366b88efa20f1afff5b293e1ca18e012b08
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 09:26:54 -0500

    update

--- pages/1_Graficos.py
+++ pages/1_Graficos.py
@@ -11,7 +11,7 @@ layout = "centered"
 
 #setting title for our app
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
-st.title(page_title + " " + page_icon)
+st.title(page_icon + " " + page_title)
 st.markdown("""
         <style>
                .block-container {
