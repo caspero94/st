5c96defa27b872a9a8ad2e93df8dcf95fd7affe6
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 13:12:12 -0500

    update

--- pages/1_Graficos.py
+++ pages/1_Graficos.py
@@ -10,7 +10,7 @@ page_icon = ":chart:"
 layout = "centered"
 
 #setting title for our app
-st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=wide,)
 st.title(page_icon + " " + page_title)
 st.markdown("""
         <style>
