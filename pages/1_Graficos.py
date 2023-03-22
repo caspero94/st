3f1ce9ce3ab37ddf35e8fb2a068fbd21294959fb
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 14:16:47 -0500

    update

--- pages/1_Graficos.py
+++ pages/1_Graficos.py
@@ -14,7 +14,10 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout,)
 st.title(page_icon + " " + page_title)
 st.markdown("""
         <style>
-               .block-container {
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                .block-container {
                     padding-top: 2rem;
                     padding-bottom: 0rem;
                     padding-left: 1rem;
