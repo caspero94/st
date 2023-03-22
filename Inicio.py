5bd1b4fc12a27d5e9ffc8eda5aedbc83f35fdb89
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 10:50:22 -0500

    update

--- Inicio.py
+++ Inicio.py
@@ -1,6 +1,6 @@
 # Resources
 import streamlit as st
-
+from menu import menu_p
 #variables
 page_title = "Proyect X"
 page_icon = ":graph-up:"
@@ -9,7 +9,6 @@ layout = "centered"
 #setting title for our app
 st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_icon + " " + page_title)
-import menu
 st.markdown("""
         <style>
                .block-container {
@@ -23,7 +22,8 @@ st.markdown("""
 
 #st.markdown("# Inicio")
 #st.sidebar.markdown("# Inicio")
-    
+
+menu_p()
 
 
 
