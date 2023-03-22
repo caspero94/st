fe4b7317452dedb969cd7e71c8d4fd5a97b861d6
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 09:18:24 -0500

    update

--- Inicio.py
+++ Inicio.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
 
-
-
 #variables
 page_title = "Proyect X"
 page_icon = ":money_with_wings:"
@@ -14,14 +12,14 @@ st.title(page_title + " " + page_icon)
 st.markdown("""
         <style>
                .block-container {
-                    padding-top: 1rem;
+                    padding-top: 2rem;
                     padding-bottom: 0rem;
                     padding-left: 0rem;
                     padding-right: 0rem;
                 }
         </style>
         """, unsafe_allow_html=True)
-st.markdown("# Inicio")
+#st.markdown("# Inicio")
 #st.sidebar.markdown("# Inicio")
 
 
