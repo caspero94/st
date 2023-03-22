b9b4629fd1fc19c37490d319274a33e1f3aa711b
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-22 17:09:05 -0500

    update

--- pages/1_Graficos.py
+++ pages/1_Graficos.py
@@ -38,7 +38,7 @@ st.markdown("""
 db = dbmongo.get_mongo_db()
 
 # Obtener collections
-col1, col2, col3 = st.columns([1,4,1])
+col1, col2, col3 = st.columns([1,5,2])
 with col1:
     par = st.selectbox(
         "Coin",
