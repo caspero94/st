ef607a01796d1086b76cab6487ac44bf6edfb147
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-24 11:06:39 -0500

    update

---
+++ pages/3_Test_Estrategia.py
@@ -0,0 +1,24 @@
+# Resources
+import streamlit as st
+
+#variables
+page_title = "Test Estragia"
+page_icon = ":bar_chart:"
+layout = "wide"
+#page_title=page_title, page_icon=page_icon,
+#setting title for our app
+st.set_page_config( layout=layout,)
+#st.title(page_icon + " " + page_title)
+st.markdown("""
+        <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                .block-container {
+                    padding-top: 2rem;
+                    padding-bottom: 0rem;
+                    padding-left: 1rem;
+                    padding-right: 2rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
