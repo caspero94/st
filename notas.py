49bdf51767893a82d8359e553a2e5287c746ba7a
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-24 09:04:08 -0500

    update

--- notas.py
+++ notas.py
@@ -110,5 +110,51 @@ timeframe ="1m"
             timeframe = "1S" 
     with me1:
         if st.button('1M '):
-            timeframe = "1M"         
+            timeframe = "1M"   
+---------------------------
+# menu.py
+
+# Resources
+import streamlit as st
+from streamlit_option_menu import option_menu
+from streamlit_extras.switch_page_button import switch_page
+# Horizontal menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Inicio","Graficos","Obtener datos"], 
+        icons=["house","graph-up","file-bar-graph-fill"], menu_icon="cast", default_index=1)
+    selected
+    
+if selected == "Inicio":
+    pass
+if selected == "Graficos":
+    switch_page("Graficos")
+
+if selected == "Obtener datos":
+    switch_page("Obtener datos")
+
+--------------------------------
+# page_config.py
+
+# Resources
+import streamlit as st
+
+#variables
+page_title = "Proyect X"
+page_icon = ":graph-up:"
+layout = "centered"
+
+#setting title for our app
+st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+st.title(page_icon + " " + page_title)
+st.markdown("""
+        <style>
+               .block-container {
+                    padding-top: 2rem;
+                    padding-bottom: 0rem;
+                    padding-left: 0rem;
+                    padding-right: 0rem;
+                }
+        </style>
+        """, unsafe_allow_html=True)
+
 '''
