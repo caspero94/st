a5a1bcb66aaddf90ea036779c45b2ea676d05b0c
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-24 09:19:32 -0500

    update

--- pages/1_Graficos.py
+++ pages/1_Graficos.py
@@ -85,11 +85,9 @@ collection = db[select_col]
 from_datetime = datetime.datetime.combine(fromdate, datetime.datetime.min.time())
 to_datetime = datetime.datetime.combine(todate, datetime.datetime.max.time())
 data_activo = pd.DataFrame(list(collection.find({'datetime': {'$gte': from_datetime, '$lte': to_datetime}})))
-st.write(data_activo)
 # Eliminamos columnas inecesarias
 data_activo.drop(['_id','timestamp'], axis=1, inplace=True)
 #data_activo = data_activo.set_index('datetime')
-st.write(data_activo)
 # Muestra el resultado en tu aplicación de Streamlit
 with st.container():
     fig = go.Figure()
