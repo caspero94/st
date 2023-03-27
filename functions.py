08c9cee9b7ff85bbd8cda27f5dda78bdb2483900
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-27 11:05:04 -0500

    update

--- functions.py
+++ functions.py
@@ -46,7 +46,7 @@ def save_candles(symbol, timeframe):
 
     # Comprobamos si hay datos previos, si hay, eliminamos el ultimo registro para actualizarlo y sino hay empezamos desde el inicio
     try:
-        last_data = pd.DataFrame(list(collection.find_one(sort=[("_id", pymongo.DESCENDING)]).limit(1)))
+        last_data = pd.DataFrame(list(collection.find_one(sort=[("_id", pymongo.DESCENDING)])))
         st.write(last_data)
         from_timestamp = int(last_data['timestamp'].iloc[0])
         st.write(from_timestamp)
