5e26601b92f13864b8076c0b1c1e00e43c2cf84b
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-28 14:24:36 -0500

    update

--- pages/1_Grafico.py
+++ pages/1_Grafico.py
@@ -109,7 +109,7 @@ if (len(data_activo)) > 0:
         st.plotly_chart(fig,use_container_width=True,config=configs)
 
 
-        '''while True:
+        while True:
             time.sleep(15)
             #save_candles(symbol = par, timeframe = timeframe)
             update_time= datetime.datetime.now() - datetime.timedelta(minutes=2)
@@ -117,7 +117,7 @@ if (len(data_activo)) > 0:
             data_activo_update = pd.DataFrame(list(collection.find({'_id': {'$gte': update_time, '$lte': update_now}})))
             data_activo = data_activo.append(data_activo_update)
             data_activo = data_activo.drop_duplicates(subset=['_id'])
-            fig.update_traces(go.Candlestick(x=data_activo["_id"], open=data_activo["open"], high=data_activo["high"], low=data_activo["low"], close=data_activo["close"]))'''
+            fig.update_traces(go.Candlestick(x=data_activo["_id"], open=data_activo["open"], high=data_activo["high"], low=data_activo["low"], close=data_activo["close"]))
                                     
 else:
     st.info("No se encontraron datos disponibles para este activo y fechas")
