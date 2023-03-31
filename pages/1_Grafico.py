74c18df1bb1d58e919279e9b69bec492a466d362
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-31 08:21:58 -0500

    update

--- pages/1_Grafico.py
+++ pages/1_Grafico.py
@@ -102,7 +102,7 @@ if (len(data_activo)) > 0:
             xaxis_rangeslider_visible=False)
         configs = dict({'scrollZoom': False,'displaylogo': False} )
         fig.add_trace(go.Candlestick(x=data_activo["datetime"], open=data_activo["open"], high=data_activo["high"], low=data_activo["low"], close=data_activo["close"]))
-        fig.add_trace(go.Histogram(x=data_activo["volume"]))
+        #fig.add_trace(go.Histogram(x=data_activo["volume"]))
         chart_placeholder.plotly_chart(fig,use_container_width=True,config=configs)
         st.write(data_activo)
         
@@ -111,10 +111,8 @@ if (len(data_activo)) > 0:
             data_activo = pd.DataFrame(list(collection.find({'_id': {'$gte': from_datetime, '$lte': to_datetime}})))
             data_activo['datetime'] = pd.to_datetime(data_activo['_id'], unit='ms')
             fig.update_traces(go.Candlestick(x=data_activo["datetime"], open=data_activo["open"], high=data_activo["high"], low=data_activo["low"], close=data_activo["close"]))
-            #fig.update_traces(go.Histogram(x=data_activo["volume"]))
-            chart_placeholder.plotly_chart(fig,use_container_width=True,config=configs)
-                
-            time.sleep(5)      
+            chart_placeholder.plotly_chart(fig,use_container_width=True,config=configs)  
+            time.sleep(6)      
                 
 else:
     st.info("No se encontraron datos disponibles para este activo y fechas")
