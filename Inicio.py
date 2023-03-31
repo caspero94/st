e3f724b1250466574e7ba5daf8ef935ddb5b8ec7
caspero94 <84985267+caspero94@users.noreply.github.com>
2023-03-31 09:23:24 -0500

    update

--- Inicio.py
+++ Inicio.py
@@ -11,15 +11,20 @@ st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
 st.title(page_icon + " " + page_title)
 st.markdown("""
         <style>
-               .block-container {
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                header {visibility: hidden;}
+                .block-container {
                     padding-top: 2rem;
                     padding-bottom: 0rem;
-                    padding-left: 0rem;
-                    padding-right: 0rem;
+                    padding-left: 1rem;
+                    padding-right: 3rem;
+                }
+                button[title="View fullscreen"] {
+                    visibility: hidden;
                 }
         </style>
         """, unsafe_allow_html=True)
-
 
 #st.markdown("# Inicio")
 #st.sidebar.markdown("# Inicio")
